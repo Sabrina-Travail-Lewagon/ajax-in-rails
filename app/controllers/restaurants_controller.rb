aaeeeb212b881e2c2f47753590970b626baa1f97
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-06-12 14:48:56 +0200

    ajout moteur recherche ajax sur index restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    if params[:query].present?
+      @restaurants = @restaurants.where('name ILIKE ?', "%#{params[:query]}%")
+
+      respond_to do |format|
+        format.html { render 'index' }
+        format.text { render partial: 'list', locals: { restaurants: @restaurants }, formats: [:html] }
+      end
+    else
+      respond_to do |format|
+        format.html { render 'index' }
+        format.text { render partial: 'list', locals: { restaurants: @restaurants }, formats: [:html] }
+      end
+    end
   end
+
+
 
   def show
     @restaurant = Restaurant.find(params[:id])
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import InsertInListController from "./insert_in_list_controller"
 application.register("insert-in-list", InsertInListController)
+
+import RefreshListController from "./refresh_list_controller"
+application.register("refresh-list", RefreshListController)
